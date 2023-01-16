e53922e5a9f5e0dde073a5c3859e01b629f825dd
Joffreynk <joyjoffrey@gmail.com>
2023-01-16 22:48:26 +0200

    fix linters

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,13 +7,13 @@ import MoviePanel from './MoviePanel';
 // 1   2  3  4  5  6  7  8  9
 
 const Carousel = () => {
-  const [range, setRange] = useState(0)
-  const newData = data.slice(range, range+3)
-  const prev = ()=>{
-    if(range+3 === data.length-1|| range === 0){
-      setRange(data.length-4)
-    } else setRange((prevV => prevV-3))
-  }
+  const [range, setRange] = useState(0);
+  const newData = data.slice(range, range + 3);
+  const prev = () => {
+    if (range + 3 === data.length - 1 || range === 0) {
+      setRange(data.length - 4);
+    } else setRange(((prevV) => prevV - 3));
+  };
 
   const next = () => {
     if (range > (data.length - 4)) {
@@ -23,9 +23,9 @@ const Carousel = () => {
 
   return (
     <div className="flex mt-72">
-      <button onClick={prev}><FcPrevious/></button>
-      {newData.map((el, i) => <MoviePanel key={i} el={el} />)}
-      <button onClick={next}><FcNext/></button>
+      <button type="button" onClick={prev}> <FcPrevious/> </button>
+      {newData.map((el, i) => <MoviePanel key={`${i * 3}`} el={el} />)}
+      <button type="button" onClick={next}><FcNext/></button>
     </div>
   )
 };
