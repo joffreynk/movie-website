da077e7ddc0d5b837e6d6644f2e3f87377e4b272
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 19:59:07 +0200

    style buttons color

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -20,22 +20,24 @@ const Movies = () => {
 
   // }
   return (
-    <div>
-      <h3 className="flex justify-center items-center">movies</h3>
-      <div className="flex">
-        <div className="flex flex-col ">
-          <h4  className="">genre</h4>
-          <div className="flex flex-col">
-          { mygenres.map((genre) => <button key={genre} onClick={() => setDisplayedMovies(moviesData.filter(movie => movie.genre.includes(genre)))}>{genre}</button>) }
+    <div className="flex flex-col gap-20">
+      <h3 className="uppercase text-transparent font-extrabold tracking-[20px] text-5xl bg-clip-text bg-gradient-to-r from-[#f5065e] to-[#0004e0]">movies</h3>
+      <div className="flex gap-8">
+        <div className="flex flex-col gap-5">
+          <h4  className="uppercase font-bold ">genre</h4>
+          <div className="flex flex-col items-start justify-start group">
+          { mygenres.map((genre) => <button className="group-active:bg-red-900" key={genre} onClick={() => setDisplayedMovies(moviesData.filter(movie => movie.genre.includes(genre)))}>{genre}</button>) }
           </div>
         </div>
-        <ul className="grid grid-cols-3 gap-x-3 gap-y-12">
-          {displayedMovies.map((movie, i) => <SingleMovie key={`${movie.imdbid}Q${movie.imdbrating}`} movie={movie} /> )}
-        </ul>
-        <div className="flex justify-center items-center">
-          <button onClick={allMovies}>{viewLess ? 'View all movies' : 'View less movies'}</button>
+        <div className="flex flex-col ">
+          <ul className="grid grid-cols-3 gap-x-3 gap-y-12">
+            {displayedMovies.map((movie, i) => <SingleMovie key={`${movie.imdbid}Q${movie.imdbrating}`} movie={movie} /> )}
+          </ul>
+          <div className="flex justify-center items-center w-full">
+            <button className="capitalize bg-gradient-to-r from-[#ef4d88] to-[#4e51ff] px-16 py-4 rounded-lg text-2xl w-full" onClick={allMovies}>{viewLess ? 'View all movies' : 'View less movies'}</button>
+          </div>
         </div>
-      </div>
+        </div>
     </div>
   )
 }
