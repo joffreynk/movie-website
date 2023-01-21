390d64967f23279096f2fa555703cdf8db713725
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 22:22:20 +0200

    add footer section

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Movies from './components/Movies';
 import Premium from './components/Premium';
 
 const App = () => (
-  <div className="relative font-opensans  bg-[#1f1f1f] text-white pb-32 capitalize">
+  <div className="relative font-opensans  bg-[#1f1f1f] text-white capitalize">
     <div className="">
       <Header />
     </div>
 
-    <div className="pt-44  bg-[#3f3f3f] pb-10">
+    <div className="pt-44  bg-[#3f3f3f]">
       <Carousel />
     </div>
-    <div className='flex justify-center items-center my-10'>
+    <div className='flex justify-center items-center '>
       <button className="capitalize bg-gradient-to-r from-[#ef4d88] to-[#4e51ff] px-16 py-4 rounded-lg text-3xl" >Subscription</button>
     </div>
       <div className="mt-10 mx-[5%]">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-center items-center py-10'>
+        <p>Movie store website developed by @<a href='https://github.com/JoffreyNK'>Joffrey</a> </p>
+    </div>
   )
 }
 
