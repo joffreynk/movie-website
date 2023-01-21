5105baaf1d2cb707918d41f64620f0fa2d5d76cc
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 18:32:46 +0200

    ADD: add view button to view all or less movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,15 +2,32 @@ import { useState } from "react"
 import moviesData from "../data/moviesData"
 import SingleMovie from "./SingleMovie"
 const Movies = () => {
+
+  const [displayedMovies, setDisplayedMovies] = useState(moviesData.slice(0, 9))
+  const [viewLess, setViewLess] = useState(true)
+
+  const allMovies = () => {
+    if(viewLess){
+      setDisplayedMovies(moviesData)
+      setViewLess(false)
+    }else {
+      setDisplayedMovies(moviesData.slice(0, 9))
+      setViewLess(true)
+    }
+  }
+
   return (
     <div>
       <h3>movies</h3>
       <div>
 
       </div>
-      <ul className="grid grid-cols-3 gap-3">
-        {moviesData.map((movie) => <SingleMovie key={movie.imdbid} movie={movie} /> )}
+      <ul className="grid grid-cols-3 gap-x-3 gap-y-12">
+        {displayedMovies.map((movie) => <SingleMovie key={movie.imdbid} movie={movie} /> )}
       </ul>
+      <div className="flex justify-center items-center">
+        <button onClick={allMovies}>{viewLess ? 'View all movies' : 'View less movies'}</button>
+      </div>
     </div>
   )
 }
