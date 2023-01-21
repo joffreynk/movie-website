fe3c8eb3dff62ab2e48cfd52dc6e3e1426407cf1
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 20:43:11 +0200

    add hovered movie descrption

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -3,15 +3,19 @@ import React from 'react'
 const SingleMovie = ({movie}) => {
 
   return (
-    <li className='flex flex-col gap-4 p-5  bg-[#3f3f3f]' >
+    <li className='flex flex-col gap-4 p-5  bg-[#3f3f3f] ' >
+      <div className="relative group">
         <div>
           <img src={movie.imageurl[0]} alt={movie.title} className="w-full" />
         </div>
+        {movie.synopsis.length>1? <p className="absolute hidden group-hover:block text-lg bottom-0 bg-gray-50 bg-opacity-90 text-[#1f1f1f] p-3 text-justify">{movie.synopsis}</p>:''}
+      </div>
         <h4>{movie.title} {movie.released ? `(${movie.released})`:''}</h4>
         <h4></h4>
         <ul className='flex justify-between gap-2'>
           {movie.genre.map(genre => <li>{genre}</li>)}
         </ul>
+        
     </li>
   )
 }
