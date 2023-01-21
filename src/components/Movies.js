8f748e22c8f9fc48946e68ed78984337dfe5588e
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 20:12:46 +0200

    fix view all button

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -26,15 +26,15 @@ const Movies = () => {
         <div className="flex flex-col gap-5">
           <h4  className="uppercase font-bold ">genre</h4>
           <div className="flex flex-col items-start justify-start group">
-          { mygenres.map((genre) => <button className="group-active:bg-red-900" key={genre} onClick={() => setDisplayedMovies(moviesData.filter(movie => movie.genre.includes(genre)))}>{genre}</button>) }
+          { mygenres.map((genre) => <button className="group-focus:bg-red-900" key={genre} onClick={() => setDisplayedMovies(moviesData.filter(movie => movie.genre.includes(genre)))}>{genre}</button>) }
           </div>
         </div>
-        <div className="flex flex-col ">
+        <div className="flex flex-col gap-20">
           <ul className="grid grid-cols-3 gap-x-3 gap-y-12">
             {displayedMovies.map((movie, i) => <SingleMovie key={`${movie.imdbid}Q${movie.imdbrating}`} movie={movie} /> )}
           </ul>
           <div className="flex justify-center items-center w-full">
-            <button className="capitalize bg-gradient-to-r from-[#ef4d88] to-[#4e51ff] px-16 py-4 rounded-lg text-2xl w-full" onClick={allMovies}>{viewLess ? 'View all movies' : 'View less movies'}</button>
+            <button className="capitalize bg-gradient-to-r from-[#ef4d88] to-[#4e51ff] py-2 rounded-lg text-lg w-full" onClick={allMovies}>{viewLess ? 'View all movies' : 'View less movies'}</button>
           </div>
         </div>
         </div>
