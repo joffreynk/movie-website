c2d7c6818c27c99e85f7579d543e4aec53b3ad35
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 11:38:07 +0200

    change slices bg color

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Movies from './components/Movies';
 import Premium from './components/Premium';
 
 const App = () => (
-  <div className="relative font-opensans " style={{ height: '200rem' }}>
+  <div className="relative font-opensans  bg-[#1f1f1f] text-white" style={{ height: '200rem' }}>
     <div className="fixed top-0 right-0 left-0 h-28  bg-[#1f1f1f]">
       <Header />
     </div>
 
-    <div className="mt-36">
+    <div className="pt-44  bg-[#3f3f3f] pb-10">
       <Carousel />
     </div>
     <div className='flex justify-center items-center my-10'>
