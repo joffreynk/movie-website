ea70120fe669ff0e5f92d1bd420594613d312be1
Joffreynk <joyjoffrey@gmail.com>
2023-01-21 18:34:01 +0200

    Fix: add full width on images sizes

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const SingleMovie = ({movie}) => {
+
   return (
-    <li className='flex flex-col gap-4 p-5  bg-[#3f3f3f] '>
+    <li className='flex flex-col gap-4 p-5  bg-[#3f3f3f]' >
         <div>
           <img src={movie.imageurl[0]} alt={movie.title} className="w-full" />
         </div>
